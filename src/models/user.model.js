2dd03574cbceec0a0617614d18812ca43798d940
SiddhantJain <siddhantjain027@gmail.com>
2024-08-27 02:57:07 +0530

    jwt/hooks  implentation

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,7 @@
 import mongoose, { mongo, Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
 
 
 const userSchema = new Schema(
@@ -51,6 +54,49 @@ const userSchema = new Schema(
         timestamps: true
     }
 )
+//we are using this hook to perform an action before the final task is achieved 
+//this is acting kind of middleware so we are using next here 
 
+userSchema.pre("save", async function(next) {
+    //password field ko leke usko encrypt karke save kardo
+    //but first check if we really want to access the password 
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+//we can design our own custom methods as well 
+userSchema.methods.isPasswordCorrect = async function(password){
+   return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+
+} //both these two are jwt tokens
+
+//jwt is like a key given to users , those users who havr that key will be given access to that token 
 
 export const User = mongoose.model('User', userSchema);
