d08a5e379ef8ccd7c48d1eac473625d8c4d08a41
SiddhantJain <siddhantjain027@gmail.com>
2024-08-27 12:48:40 +0530

    code for uploading files on local storgar using multr

--- src/utils/Cloudinary.js
+++ src/utils/Cloudinary.js
@@ -3,15 +3,35 @@ import fs from "fs";
 
 // Configuration
 cloudinary.config({ 
-    cloud_name: 'dcbqepu2h', 
-    api_key: '151148522149379', 
-    api_secret: '<your_api_secret>' // Click 'View API Keys' above to copy your API secret
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        //file has been uploaded
+        console.log("File is uploaded on cloudinary", response.url);
+        return response; 
+    } catch (error) {
+        fs.unlinkSync(localFilePath); //remove the locally saved temp file 
+        return null; 
+    }
+}
+
+export {uploadOnCloudinary};
 
 
     //how to upload a file in production grade setting 
     //we use multer 
     //first we bring file to our local storage and then upload it to the file here server 
     //dont directly upload it to server 
+
+
+    
     
